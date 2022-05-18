769087ca19fc62d591d693e2721c01876928f52e
Haleem Hamid <62084047+HaleemH@users.noreply.github.com>
2022-05-18 13:58:03 -0700

    Time: 73 ms (64.43%), Space: 42.6 MB (21.27%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,14 +4,16 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+    //spread array of arrays into variable array
     const arr = [];
     for(var i=0; i<matrix.length; i++){
         arr.push(...matrix[i])
     }
+    //assign pointers
     let start= 0;
     let end= arr.length-1;
     let middle= Math.floor((start + end) / 2);
-    
+    //binary search for "target"
     while(arr[middle] !== target && start <= end){
         if(arr[middle] < target){
             start = middle+1
@@ -20,7 +22,7 @@ var searchMatrix = function(matrix, target) {
         }
         middle= Math.floor((start + end) / 2)
     }
-
+    //conditional to return answer
     if(arr[middle] === target){
         return true;
     }else return false
