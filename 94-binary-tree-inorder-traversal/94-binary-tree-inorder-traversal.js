529735578c07c92968cbcdef3f928ccb158cb4da
Haleem Hamid <62084047+HaleemH@users.noreply.github.com>
2022-05-18 11:28:16 -0700

    Time: 83 ms (39.44%), Space: 42.4 MB (36.06%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -11,13 +11,48 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    if (root === null) return []
-    const result = [];
-    function traverse(node){
-        if(node.left) traverse(node.left);
-        result.push(node.val)
-        if(node.right) traverse(node.right);
+    //recursice
+    
+    // if (root === null) return []
+    // const result = [];
+    // function traverse(node){
+    //     if(node.left) traverse(node.left);
+    //     result.push(node.val)
+    //     if(node.right) traverse(node.right);
+    // }
+    // traverse(root)
+    // return result
+    
+    //iteratively
+    
+    if(root === null) return [];
+    let node = root;
+    const result = []
+    
+    while(node){
+        if(!node.left){
+            result.push(node.val)
+            node= node.right;
+        }else{
+            const prev = findPrev(node)
+            if(prev.right === node){
+                prev.right = null
+                result.push(node.val)
+                node = node.right;
+            }else{
+                prev.right = node
+                node = node.left
+            }
+        }
     }
-    traverse(root)
+    
     return result
 };
+
+function findPrev(root){
+    let node = root.left
+    while(node.right && node.right!== root){
+        node = node.right
+    }
+    return node
+}
