762237f5e2ebc410975f438e24ce1ed01760684e
Haleem Hamid <62084047+HaleemH@users.noreply.github.com>
2022-06-03 08:54:42 -0700

    Time: 112 ms (82.57%), Space: 48.3 MB (87.71%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    /**
+    -nums = [int sorted non-decreasing order]
+    -return an Arr of the squares of each number sorted in non decreasing order
+    */
+    const squared = [];
+    for (let i = 0; i < nums.length; i++){
+        squared.push(nums[i]*nums[i])
+    }
+    return squared.sort((a,b) => {
+        if(a < b){
+            return -1;
+        }else if(a > b){
+            return 1;
+        }else return 0
+    })
+};
