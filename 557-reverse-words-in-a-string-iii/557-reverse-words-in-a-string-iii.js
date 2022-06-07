d0d8ef22163bdd648083ecbcb95e93470126e75a
Haleem Hamid <62084047+HaleemH@users.noreply.github.com>
2022-06-07 15:55:53 -0700

    Time: 71 ms (96.46%), Space: 49.5 MB (11.06%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s = s.split('').reverse().join('');
+    s = s.split(' ').reverse().join(' ');
+    
+    return s;
+};
+
